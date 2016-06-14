d7da00d361e925041944dc2a7a6b0144f98b5982
Weerasak Chongnguluam <singpor@gmail.com>
2016-06-14 17:57:26 +0700

    Extract Logic and manging Dom

--- main.go
+++ main.go
@@ -3,19 +3,15 @@ package main
 import (
 	"fmt"
 
-	"honnef.co/go/js/console"
 	"honnef.co/go/js/dom"
 )
 
-func UpdateDisplay(numChan chan int) {
-	for {
-		select {
-		case num := <-numChan:
-			console.Log("UpdateDisplay")
-			dom.GetWindow().Document().QuerySelector("#num").SetTextContent(fmt.Sprint(num))
-		}
-	}
-}
+type CounterEvent int
+
+const (
+	DownEvent CounterEvent = iota
+	UpEvent
+)
 
 type NumState struct {
 	Num int
@@ -29,20 +25,46 @@ func (n *NumState) Sub() {
 	n.Num--
 }
 
+func ReceiveCouterEvent(eventChan chan CounterEvent, numChan chan int, numState *NumState) {
+	for {
+		switch <-eventChan {
+		case UpEvent:
+			numState.Add()
+		case DownEvent:
+			numState.Sub()
+		}
+		numChan <- numState.Num
+	}
+}
+
+func UpEventListener(eventChan chan CounterEvent) {
+	dom.GetWindow().Document().QuerySelector("#up").AddEventListener("click", false, func(e dom.Event) {
+		eventChan <- UpEvent
+	})
+}
+
+func DownEventListener(eventChan chan CounterEvent) {
+	dom.GetWindow().Document().QuerySelector("#down").AddEventListener("click", false, func(e dom.Event) {
+		eventChan <- DownEvent
+	})
+}
+
+func UpdateDisplay(numChan chan int) {
+	for {
+		select {
+		case num := <-numChan:
+			dom.GetWindow().Document().QuerySelector("#num").SetTextContent(fmt.Sprint(num))
+		}
+	}
+}
+
 func main() {
 	var numState NumState
+	eventChan := make(chan CounterEvent, 1)
 	numChan := make(chan int, 1)
 
 	go UpdateDisplay(numChan)
-
-	dom.GetWindow().Document().QuerySelector("#up").AddEventListener("click", false, func(e dom.Event) {
-		numState.Add()
-		console.Log(numState.Num)
-		numChan <- numState.Num
-	})
-
-	dom.GetWindow().Document().QuerySelector("#down").AddEventListener("click", false, func(e dom.Event) {
-		numState.Sub()
-		numChan <- numState.Num
-	})
+	go ReceiveCouterEvent(eventChan, numChan, &numState)
+	UpEventListener(eventChan)
+	DownEventListener(eventChan)
 }
